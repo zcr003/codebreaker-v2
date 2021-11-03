ee25eb559af09696313e05be3bda924ede57aab7
Nick Bennett <nick@nickbenn.com>
2021-11-03 15:06:25 -0600

    Started scores layout with recycler view.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/ScoresFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/ScoresFragment.java
@@ -4,36 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import edu.cnm.deepdive.codebreaker.databinding.FragmentScoresBinding;
 import edu.cnm.deepdive.codebreaker.viewmodel.ScoresViewModel;
-import edu.cnm.deepdive.codebreaker.databinding.FragmentGalleryBinding;
 
 public class ScoresFragment extends Fragment {
 
-  private ScoresViewModel scoresViewModel;
-  private FragmentGalleryBinding binding;
+  private ScoresViewModel viewModel;
+  private FragmentScoresBinding binding;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
-    scoresViewModel =
-        new ViewModelProvider(this).get(ScoresViewModel.class);
+    binding = FragmentScoresBinding.inflate(inflater, container, false);
+    return binding.getRoot();
+  }
 
-    binding = FragmentGalleryBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
-
-    final TextView textView = binding.textGallery;
-    scoresViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-      @Override
-      public void onChanged(@Nullable String s) {
-        textView.setText(s);
-      }
-    });
-    return root;
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    viewModel = new ViewModelProvider(this).get(ScoresViewModel.class);
+    viewModel
+        .getGames()
+        .observe(getViewLifecycleOwner(), (games) -> {
+          // TODO Create adapter with games, and attach adapter to RecyclerView.
+        });
   }
 
   @Override
@@ -41,4 +38,5 @@ public class ScoresFragment extends Fragment {
     super.onDestroyView();
     binding = null;
   }
+
 }
