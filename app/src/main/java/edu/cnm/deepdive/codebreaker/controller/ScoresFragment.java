7ca1707c8a03300580c017b8b50f10d2d3033b04
Nick Bennett <nick@nickbenn.com>
2021-11-24 12:26:17 -0700

    Live-refresh scores fragment in progress.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/ScoresFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/ScoresFragment.java
@@ -4,20 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.adapter.GameSummaryAdapter;
 import edu.cnm.deepdive.codebreaker.databinding.FragmentScoresBinding;
 import edu.cnm.deepdive.codebreaker.model.view.GameSummary;
 import edu.cnm.deepdive.codebreaker.viewmodel.ScoresViewModel;
 import java.util.List;
+import java.util.function.BiConsumer;
 
-public class ScoresFragment extends Fragment {
+public class ScoresFragment extends Fragment implements SeekBar.OnSeekBarChangeListener {
 
   private ScoresViewModel viewModel;
   private FragmentScoresBinding binding;
+  private BiConsumer<Integer, Boolean> codeLengthUpdater;
+  private BiConsumer<Integer, Boolean> poolSizeUpdater;
+  private BiConsumer<Boolean, Boolean> sortedByTimeUpdater;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
@@ -27,6 +33,11 @@ public class ScoresFragment extends Fragment {
     // TODO Add onItemSelectedListener to spinner. Listener will invoke set methods in
     //  ScoresViewModel to force a refresh of the query.
     // TODO Populate spinners, using min and max code length & pool size integer resources.
+    setupParameterChangeConsumers();
+    binding.codeLength.setTag(codeLengthUpdater);
+    binding.poolSize.setTag(poolSizeUpdater);
+    binding.codeLength.setOnSeekBarChangeListener(this);
+    binding.poolSize.setOnSeekBarChangeListener(this);
     return binding.getRoot();
   }
 
@@ -55,6 +66,46 @@ public class ScoresFragment extends Fragment {
   public void onDestroyView() {
     super.onDestroyView();
     binding = null;
+  }
+
+  private void setupParameterChangeConsumers() {
+    codeLengthUpdater = (value, fromUser) -> {
+      binding.codeLengthDisplay.setText(String.valueOf(value));
+      if (fromUser) {
+        viewModel.setCodeLength(value);
+      }
+    };
+    poolSizeUpdater = (value, fromUser) -> {
+      binding.poolSizeDisplay.setText(String.valueOf(value));
+      if (fromUser) {
+        viewModel.setPoolSize(value);
+      }
+    };
+    sortedByTimeUpdater = (sortedByTime, fromUser) -> {
+      if (sortedByTime) {
+        binding.header.time.setText(R.string.time_header_selected);
+        binding.header.guesses.setText(R.string.guesses_header_unselected);
+      } else {
+        binding.header.time.setText(R.string.time_header_unselected);
+        binding.header.guesses.setText(R.string.guesses_header_selected);
+      }
+      if (fromUser) {
+        viewModel.setSortedByTime(sortedByTime);
+      }
+    };
+  }
+
+  @Override
+  public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+    ((BiConsumer<Integer, Boolean>) seekBar.getTag()).accept(progress, fromUser);
+  }
+
+  @Override
+  public void onStartTrackingTouch(SeekBar seekBar) {
+  }
+
+  @Override
+  public void onStopTrackingTouch(SeekBar seekBar) {
   }
 
 }
