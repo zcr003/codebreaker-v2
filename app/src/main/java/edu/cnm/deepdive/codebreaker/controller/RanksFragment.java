e461ea2752f9983d1deb55ad2816e2e7c117ba40
zcr003 <zramsey1@cnm.edu>
2021-12-07 14:12:18 -0700

    Hooked up navigation & menu for new fragment.

---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/RanksFragment.java
@@ -0,0 +1,100 @@
+package edu.cnm.deepdive.codebreaker.controller;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import edu.cnm.deepdive.codebreaker.R;
+import edu.cnm.deepdive.codebreaker.databinding.FragmentRanksBinding;
+import edu.cnm.deepdive.codebreaker.viewmodel.ScoresViewModel;
+
+public class RanksFragment extends Fragment {
+
+  private ScoresViewModel viewModel;
+  private FragmentRanksBinding binding;
+
+  public View onCreateView(@NonNull LayoutInflater inflater,
+      ViewGroup container, Bundle savedInstanceState) {
+    binding = FragmentRanksBinding.inflate(inflater, container, false);
+    binding.codeLength.setOnSeekBarChangeListener(
+        (SimpleChangeListener) this::handleCodeLengthChange);
+    binding.poolSize.setOnSeekBarChangeListener((SimpleChangeListener) this::handlePoolSizeChange);
+    binding.header.time.setOnClickListener((v) -> handleSortedByTimeChange(true, true));
+    binding.header.guesses.setOnClickListener((v) -> handleSortedByTimeChange(false, true));
+    return binding.getRoot();
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    viewModel = new ViewModelProvider(this).get(ScoresViewModel.class);
+    viewModel
+        .getRankings()
+        .observe(getViewLifecycleOwner(), (ranks) -> {
+//          GameSummaryAdapter adapter = new GameSummaryAdapter(getContext(), games);
+//          binding.games.setAdapter(adapter);
+        });
+    viewModel
+        .getCodeLength()
+        .observe(getViewLifecycleOwner(),
+            (codeLength) -> binding.codeLength.setProgress(codeLength));
+    viewModel
+        .getPoolSize()
+        .observe(getViewLifecycleOwner(), (poolSize) -> binding.poolSize.setProgress(poolSize));
+    viewModel
+        .getSortedByTime()
+        .observe(getViewLifecycleOwner(),
+            (sortedByTime) -> handleSortedByTimeChange(sortedByTime, false));
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    binding = null;
+  }
+
+  private void handleCodeLengthChange(View view, int value, boolean fromUser) {
+    binding.codeLengthDisplay.setText(String.valueOf(value));
+    if (fromUser) {
+      viewModel.setCodeLength(value);
+    }
+  }
+
+  private void handlePoolSizeChange(View view, int value, boolean fromUser) {
+    binding.poolSizeDisplay.setText(String.valueOf(value));
+    if (fromUser) {
+      viewModel.setPoolSize(value);
+    }
+  }
+
+  private void handleSortedByTimeChange(boolean sortedByTime, boolean fromUser) {
+    if (sortedByTime) {
+      binding.header.time.setText(R.string.time_header_selected);
+      binding.header.guesses.setText(R.string.guesses_header_unselected);
+    } else {
+      binding.header.time.setText(R.string.time_header_unselected);
+      binding.header.guesses.setText(R.string.guesses_header_selected);
+    }
+    if (fromUser) {
+      viewModel.setSortedByTime(sortedByTime);
+    }
+  }
+
+  @FunctionalInterface
+  private interface SimpleChangeListener extends SeekBar.OnSeekBarChangeListener {
+
+    @Override
+    default void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    default void onStopTrackingTouch(SeekBar seekBar) {
+    }
+
+  }
+}
